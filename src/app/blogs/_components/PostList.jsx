bb1011a3fb3c5bc6a7706850575593ebae2f8279
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2025-01-24 04:09:14 +0330

    *Image Component & Post Content/Interaction

--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
+import CoverImage from "./CoverImage";
+import Author from "./Author";
+import StudyTime from "./StudyTime";
+import PostInteraction from "./PostInteraction";
+
 async function PostList() {
-  await new Promise((res, rej) => setTimeout(() => res(), 3000));
+  await new Promise((res, rej) => setTimeout(() => res(), 1500));
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
   const {
@@ -7,9 +13,27 @@ async function PostList() {
   } = await res.json();
 
   return (
-    <div>
-      {posts.length > 0 && posts.map((post) => <div>{post.title}</div>)}
-    </div>
+    posts.length > 0 && (
+      <div className="grid grid-cols-12 gap-8">
+        {posts.map((post) => (
+          <div className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-200 p-2 rounded-lg">
+            <CoverImage {...post} />
+            <div>
+              <Link href={`/blogs/${post.slug}`}>
+                <h2 className="mb-4 font-bold text-secondary-700">
+                  {post.title}
+                </h2>
+              </Link>
+              <div className="flex items-center justify-between mb-4">
+                <Author {...post.author} />
+                <StudyTime {...post} />
+              </div>
+              <PostInteraction post={post} />
+            </div>
+          </div>
+        ))}
+      </div>
+    )
   );
 }
 
