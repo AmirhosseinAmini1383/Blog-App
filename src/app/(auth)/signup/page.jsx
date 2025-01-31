605b39a42cf2a0fe7678072486057b62ed09b459
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2025-01-31 13:55:00 +0330

    signup Page

---
+++ src/app/(auth)/signup/page.jsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Button from "@/ui/Button";
+import RHFTextField from "@/ui/RHFTextField";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+// export const metadata = {
+//   title: "ثبت نام",
+// };
+
+const schema = yup
+  .object({
+    name: yup
+      .string()
+      .min(5, "حداقل ۵ کاراکتر وارد کنید")
+      .max(30, "حداکثر میتوانید ۳۰ کاراکتر وارد کنید")
+      .required("نام و نام خانوادگی الزامی است"),
+    email: yup.string().email("ایمیل نامعتبر است").required("ایمیل الزامی است"),
+    password: yup.string().required("رمز عبور الزامی است"),
+  })
+  .required();
+
+function SingUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isLoading },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <div>
+      <h1 className="text-xl font-bold text-secondary-500 text-center mb-6">
+        ثبت نام
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-10">
+        <RHFTextField
+          name="name"
+          label="نام و نام خانوادگی"
+          register={register}
+          isRequired
+          errors={errors}
+        />
+        <RHFTextField
+          name="email"
+          label="ایمیل"
+          register={register}
+          dir="ltr"
+          isRequired
+          errors={errors}
+        />
+        <RHFTextField
+          name="password"
+          label="رمز عبور"
+          register={register}
+          type="password"
+          dir="ltr"
+          isRequired
+          errors={errors}
+        />
+        <Button type="submit" variant="primary" className="w-full">
+          تایید
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default SingUp;
--- src/app/(blogs)/blogs/(PostList)/layout.jsx
+++ src/app/(blogs)/blogs/(PostList)/layout.jsx
@@ -12,7 +12,9 @@ export const metadata = {
 function Layout({ children }) {
   return (
     <div>
-      <h1 className="text-lg font-bold mb-12">لیست بلاگ ها</h1>
+      <h1 className="text-lg font-bold mb-12 text-secondary-800">
+        لیست بلاگ ها
+      </h1>
       <div className="grid grid-cols-12 gap-8">
         <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-500 space-y-4">
           <Suspense fallback={<Loading />}>
