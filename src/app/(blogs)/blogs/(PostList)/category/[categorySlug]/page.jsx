506d312a29ba83bcb6d51204878232f9514071ab
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2025-02-05 01:48:00 +0330

    * Adding SearchQuery to URL * & * Fetching Based on URL Queries * => search on blogs and category Page

--- src/app/(blogs)/blogs/(PostList)/category/[categorySlug]/page.jsx
+++ src/app/(blogs)/blogs/(PostList)/category/[categorySlug]/page.jsx
@@ -1,24 +1,41 @@
+import { getPosts } from "@/services/postService";
+import { toPersianDigits } from "@/utils/numberFormatter";
+import setCookieOnReq from "@/utils/setCookieOnReq";
+import { cookies } from "next/headers";
+import queryString from "query-string";
+
 const { default: PostList } = require("app/(blogs)/blogs/_components/PostList");
 
-async function Category({ params }) {
+async function Category({ params, searchParams }) {
   const { categorySlug } = await params;
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/post/list?categorySlug=${categorySlug}`
-  );
-  const { data } = await res.json();
-  const { posts } = data || {};
+  const queries = `${queryString.stringify(
+    await searchParams
+  )}&categorySlug=${categorySlug}`;
+  const cookieStore = cookies();
+  const options = setCookieOnReq(cookieStore);
+  const posts = await getPosts(queries, options);
+
+  const { search } = await searchParams;
 
   return (
-    <div>
-      {posts.length === 0 ? (
-        <p className="text-lg text-secondary-600">
-          پستی در این دسته بندی پیدا نشد
+    <>
+      {search ? (
+        <p className="mb-4 text-secondary-700">
+          {posts.length === 0
+            ? "هیچ پستی با این مشخصات پیدا نشد"
+            : `نشان دادن ${toPersianDigits(posts.length)} نتیجه برای `}
+          <span className="font-bold">&quot;{search}&quot;</span>
         </p>
       ) : (
-        <PostList posts={posts} />
+        posts.length === 0 && (
+          <p className="text-lg text-secondary-600">
+            پستی در این دسته بندی پیدا نشد
+          </p>
+        )
       )}
-    </div>
+      <PostList posts={posts} />
+    </>
   );
 }
 
--- src/app/(blogs)/blogs/(PostList)/page.jsx
+++ src/app/(blogs)/blogs/(PostList)/page.jsx
@@ -2,16 +2,33 @@ import { cookies } from "next/headers";
 import PostList from "../_components/PostList";
 import setCookieOnReq from "@/utils/setCookieOnReq";
 import { getPosts } from "@/services/postService";
+import queryString from "query-string";
+import { toPersianDigits } from "@/utils/numberFormatter";
 
-async function BlogPage() {
+async function BlogPage({ searchParams }) {
+  const queries = queryString.stringify(await searchParams);
   const cookieStore = cookies();
   const options = setCookieOnReq(cookieStore);
-  const posts = await getPosts(options);
+  const posts = await getPosts(queries, options);
+
+  const { search } = await searchParams;
+
   return (
-    <div>
+    <>
+      {search ? (
+        <p className="mb-4 text-secondary-700">
+          {posts.length === 0
+            ? "هیچ پستی با این مشخصات پیدا نشد"
+            : `نشان دادن ${toPersianDigits(posts.length)} نتیجه برای `}
+          <span className="font-bold">&quot;{search}&quot;</span>
+        </p>
+      ) : null}
       <PostList posts={posts} />
-    </div>
+    </>
   );
 }
 
 export default BlogPage;
+
+// FOR ENGLISH TEXT :
+// showing 3 results for "folan"
