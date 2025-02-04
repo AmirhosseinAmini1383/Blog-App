d2b5002a800b8d8e66249d494ec97bd2811b99bf
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2025-02-04 20:19:52 +0330

    create Category Page & change Fetching posts api (send post Props to PostList component)

--- src/app/(blogs)/blogs/_components/PostList.jsx
+++ src/app/(blogs)/blogs/_components/PostList.jsx
@@ -3,15 +3,8 @@ import CoverImage from "./CoverImage";
 import Author from "./Author";
 import StudyTime from "./StudyTime";
 import PostInteraction from "./PostInteraction";
-import { getPosts } from "@/services/postService";
-import setCookieOnReq from "@/utils/setCookieOnReq";
-import { cookies } from "next/headers";
 
-async function PostList() {
-  const cookieStore = cookies();
-  const options = setCookieOnReq(cookieStore);
-  const posts = await getPosts(options);
-
+async function PostList({ posts }) {
   return (
     posts.length > 0 && (
       <div className="grid grid-cols-12 gap-8">
