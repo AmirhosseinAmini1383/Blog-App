990a642d5d1d70e85beeddace526a711c2f62616
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2025-02-15 23:47:11 +0330

    Create Edit Page

--- src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
+++ src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
@@ -10,7 +10,7 @@ export function CreatePost() {
       className="justify-self-end flex gap-x-4 py-3 items-center rounded-lg bg-primary-900 px-4 text-sm font-medium text-white 
       transition-colors hover:bg-primary-700"
     >
-      <span className="hidden md:block">ایجاد پست جدید</span>
+      <span className="hidden md:block">ایجاد پست</span>
       <PlusIcon className="w-5" />
     </Link>
   );
